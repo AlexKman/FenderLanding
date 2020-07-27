4171066785cc47731bfe5cb5ba7acf76997bca50
Desktopy12 <Keeley.lp@gmail.com>
2020-07-27 15:49:32 +0100

    Imported modules for routing within react and created blank files for pages Contact and Home

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <p>1</p>
-      </div>
+      <React.Fragment>
+        <Router></Router>
+      </React.Fragment>
     );
   }
 }
